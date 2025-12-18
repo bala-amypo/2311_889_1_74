63cd5a2b1d0b36196dcc90495e6a6e54a0e434da
bala-amypo <bala@amypo.in>
2025-12-18 09:19:57 +0000

    User commit

--- src/main/java/com/example/demo/model/FraudRule.java
+++ src/main/java/com/example/demo/model/FraudRule.java
@@ -1,5 +1,66 @@
 package com.example.demo.model;
-
+import 
 public class FraudRule{
-
+    @Id
+    private Long id;
+    private String ruleName;
+    private String conditionField;
+    private String value;
+    private String LOW;
+    private String MEDIUM;
+    private String HIGH;
+    
+    public FraudRule(Long id, String ruleName, String conditionField, String value, String lOW, String mEDIUM,
+            String hIGH) {
+        this.id = id;
+        this.ruleName = ruleName;
+        this.conditionField = conditionField;
+        this.value = value;
+        LOW = lOW;
+        MEDIUM = mEDIUM;
+        HIGH = hIGH;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getRuleName() {
+        return ruleName;
+    }
+    public void setRuleName(String ruleName) {
+        this.ruleName = ruleName;
+    }
+    public String getConditionField() {
+        return conditionField;
+    }
+    public void setConditionField(String conditionField) {
+        this.conditionField = conditionField;
+    }
+    public String getValue() {
+        return value;
+    }
+    public void setValue(String value) {
+        this.value = value;
+    }
+    public String getLOW() {
+        return LOW;
+    }
+    public void setLOW(String lOW) {
+        LOW = lOW;
+    }
+    public String getMEDIUM() {
+        return MEDIUM;
+    }
+    public void setMEDIUM(String mEDIUM) {
+        MEDIUM = mEDIUM;
+    }
+    public String getHIGH() {
+        return HIGH;
+    }
+    public void setHIGH(String hIGH) {
+        HIGH = hIGH;
+    }
+    
 }
