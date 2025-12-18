541c848e36f3b24eb196642b7ff490c5fab0f738
bala-amypo <bala@amypo.in>
2025-12-18 09:04:57 +0000

    User commit

--- src/main/java/com/example/demo/model/Claim.java
+++ src/main/java/com/example/demo/model/Claim.java
@@ -1,6 +1,57 @@
 package com.example.demo.model;
+import java.time.LocalDate;
+import jakarte.persistence.Id;
 
 public class Claim{
+    @Id
     private Long id;
-    private LocalDate 
+    private LocalDate claimDate;
+    private Double claimDate;
+    private String description;
+    private String PENDING;
+    private String APPROVED;
+    private String REJECTED;
+
+     public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public LocalDate getClaimDate() {
+        return claimDate;
+    }
+    public void setClaimDate(LocalDate claimDate) {
+        this.claimDate = claimDate;
+    }
+    public LocalDate getClaimDate() {
+        return claimDate;
+    }
+    public void setClaimDate(LocalDate claimDate) {
+        this.claimDate = claimDate;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getPENDING() {
+        return PENDING;
+    }
+    public void setPENDING(String pENDING) {
+        PENDING = pENDING;
+    }
+    public String getAPPROVED() {
+        return APPROVED;
+    }
+    public void setAPPROVED(String aPPROVED) {
+        APPROVED = aPPROVED;
+    }
+    public String getREJECTED() {
+        return REJECTED;
+    }
+    public void setREJECTED(String rEJECTED) {
+        REJECTED = rEJECTED;
+    }
 }
