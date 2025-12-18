b7deb01a7c93b9696ae2fe5bf258459263cec5c1
bala-amypo <bala@amypo.in>
2025-12-18 08:55:07 +0000

    User commit

--- src/main/java/com/example/demo/model/Policy.java
+++ src/main/java/com/example/demo/model/Policy.java
@@ -10,5 +10,35 @@ public class Policy{
     private String policyNumber;
     private LocalDte startDate;
     private LocalDate endDate;
-    
+
+    public Policy(Long id, String policyNumber, LocalDte startDate, LocalDate endDate) {
+        this.id = id;
+        this.policyNumber = policyNumber;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getPolicyNumber() {
+        return policyNumber;
+    }
+    public void setPolicyNumber(String policyNumber) {
+        this.policyNumber = policyNumber;
+    }
+    public LocalDte getStartDate() {
+        return startDate;
+    }
+    public void setStartDate(LocalDte startDate) {
+        this.startDate = startDate;
+    }
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
 }
