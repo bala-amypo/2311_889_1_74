e242e6342e495d5c3043fdf5fca0990d6781dcfc
bala-amypo <bala@amypo.in>
2025-12-18 17:39:36 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,20 +1,22 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.BeanRegistry.Spec;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+
 
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 public class AuthController {
-
+    @Autowired
+    AuthController src;
     @PostMapping("/register")
     public User postdata(@RequestBody User st){
         return src.savedata(st);
     }
 
-    @PostMapping("/login"){
+    @PostMapping("/login")
         public User postdata(@RequestBody User st){
             return src.savedata(st);
-        }
-    }    
+    }
 }
