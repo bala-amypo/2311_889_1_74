d0b7a1400256815ef6fd38abe30e71f9732caab8
bala-amypo <bala@amypo.in>
2025-12-18 08:49:57 +0000

    User commit

--- src/main/java/com/example/demo/model/Policy.java
+++ src/main/java/com/example/demo/model/Policy.java
@@ -1,10 +1,14 @@
 package com.example.demo.model;
 
 import jakarta.persistence.Id;
+import jakarta.persistence.Column;
 public class Policy{
     @Id
     private Long id;
     private 
+    @Column(unique = true)
     private String policyNumber;
-
+    private LocalDte startDate;
+    private LocalDate endDate;
+    
 }
