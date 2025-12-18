dbec308e9201b7fe5edc9297ed5ede3bc507455a
bala-amypo <bala@amypo.in>
2025-12-18 17:48:36 +0000

    User commit

--- src/main/java/com/example/demo/controller/PolicyCotntroller.java
+++ src/main/java/com/example/demo/controller/PolicyCotntroller.java
@@ -1,5 +1,26 @@
 package com.example.demo.controller;
 
-public class PolicyController{
+import org.springframework.beans.factory.BeanRegistry.Spec;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+@RestController
+public class PolicyController {
+    @Autowired
+    Policy src;
+    @PostMapping("/post")
+    public Policy postdata(@RequestBody Policy st){
+        return src.savedata(st);
+    }
+
+    @GetMapping("/user/{userId}")
+    public Policy getIdVal(@PathVariable int id){
+        return src.id(id);
+    }
     
 }
