d59a88b906c79c3f123ba3d18028022389e2a0a0
bala-amypo <bala@amypo.in>
2025-12-20 14:57:23 +0000

    User commit

--- src/main/java/com/example/demo/repository/ClaimRepo.java
+++ src/main/java/com/example/demo/repository/ClaimRepo.java
@@ -1,9 +1,10 @@
-=package com.example.demo.repository;
+package com.example.demo.repository;
 
 import com.example.demo.model.Claim;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ClaimRepo extends JpaRepository<Claim, Long> {
+
     Optional<Claim> findById(Long id);
 }
