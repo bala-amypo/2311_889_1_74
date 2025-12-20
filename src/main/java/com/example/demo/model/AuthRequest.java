faf679982d9fac7576f9429f167f504cb255d660
bala-amypo <bala@amypo.in>
2025-12-20 14:39:23 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/AuthRequest.java
@@ -0,0 +1,9 @@
+package com.example.demo.dto;
+
+public class AuthRequest {
+    private String email;
+    private String password;
+
+    public String getEmail() { return email; }
+    public String getPassword() { return password; }
+}
