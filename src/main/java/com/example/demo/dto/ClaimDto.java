c4438a1e6338b5a4e032d4445032cd1a1e20b84f
bala-amypo <bala@amypo.in>
2025-12-21 08:16:31 +0000

    User commit

--- src/main/java/com/example/demo/dto/ClaimDto.java
+++ src/main/java/com/example/demo/dto/ClaimDto.java
@@ -1,7 +1,22 @@
 package com.example.demo.dto;
 
+import java.time.LocalDate;
+
 public class ClaimDto {
-    public Long policyId;
-    public Double amount;
-    public String description;
+
+    private LocalDate claimDate;
+    private Double claimAmount;
+    private String description;
+
+    public LocalDate getClaimDate() {
+        return claimDate;
+    }
+
+    public Double getClaimAmount() {
+        return claimAmount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
